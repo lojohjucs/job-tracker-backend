5f6c53a0dabd8c4e6916d50b11ded794c7c037a4
Lorenzo <lojohju1997@gmail.com>
2026-09-23 10:30:36 -0500

    Prepare for deployment: gunicorn, Procfile, ALLOWED_HOSTS, DEBUG off

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#dt(=2x&jij$fomhm^sd9-k7=t3w6@g_7nxc4z$_uhmmfvckh5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
